05071956fb9bf9ba01cfc86739b56e92139caa79
rob228rob <120504923+rob228rob@users.noreply.github.com>
2024-06-23 21:37:16 +0300

    Update server.bat
    adding build command

--- server.bat
+++ server.bat
@@ -2,15 +2,22 @@
 
 if not exist node_modules (
     echo Установка зависимостей...
-    npm install  // Установка зависимостей Node.js
+    npm install
 )
 
 if not exist build\Release\addon.node (
     echo Сборка проекта...
-    node-gyp configure build  // Конфигурация и сборка проекта с помощью node-gyp
+    node-gyp configure build
 )
 
-echo starting server
+if not exist build (
+    npm run build
+    timeout /t 12
+)
+
+echo Server starting
 start http://localhost:3000
+npm start
 node index.js
+
 pause
